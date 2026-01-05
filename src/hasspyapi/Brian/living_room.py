440987669a9e02a01c033036eac9a599b6698645
Brian Christner <at.echo.off@outlook.com>
2026-01-05 12:36:10 -0600

    Debugging

--- src/hasspyapi/Brian/living_room.py
+++ src/hasspyapi/Brian/living_room.py
@@ -9,8 +9,9 @@ class LivingRoom:
         self.listener = listener
 
         self.main_ceiling_light_ip = "192.168.1.203"
-
+        print("Starting filter")
         self.lights = home.please().filter(area="Living Room", type="light", name="!Living Room Ceiling")
+        print("Filter done")
         self.default_light_settings = {"color_temp_kelvin": 2500, "brightness": 255}
         self.off_default_light_settings = {"color_temp_kelvin": 2500, "brightness": 255}
         print(self.lights.devices)
