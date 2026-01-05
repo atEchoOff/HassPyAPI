18dc4fac3ef91b3637c0f44e9c45458884065ea3
Brian Christner <at.echo.off@outlook.com>
2026-01-05 12:38:48 -0600

    Debugging

--- src/hasspyapi/hass_command.py
+++ src/hasspyapi/hass_command.py
@@ -29,8 +29,8 @@ class HassCommand:
         devices = []
         for device in self.devices:
             for kwarg in kwargs:
-                print(device)
-                print(device[kwarg])
+                # print(device)
+                # print(device[kwarg])
                 if not device[kwarg]:
                     # Device doesnt have the kwarg, so it cannot match
                     print("Not there")
