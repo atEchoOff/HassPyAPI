8145166f5d051e8d2fef192b23b835001a406606
Brian Christner <at.echo.off@outlook.com>
2026-01-05 12:48:01 -0600

    Debugging

--- src/hasspyapi/home.py
+++ src/hasspyapi/home.py
@@ -105,9 +105,6 @@ class Home:
             # Selection is listed in order of preference
             # 1. The user changed the name manually in hass (saved in "name")
             # 2. There is an existing friendly name (saved in "original_name")
-            if entity_id == "light.fireplace":
-                print(ent)
-                print(device)
             display_name = ent.get("name") or ent.get("original_name") or device.get("name_by_user") or device.get("name")
 
             if display_name and "DEPRECATED" in display_name:
